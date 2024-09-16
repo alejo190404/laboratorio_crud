1157bd941424e7081b4f285b991c5ed5c196285f
alejo190404 <alejo190404@gmail.com>
2024-09-15 19:42:35 -0500

    Configuración para servidor MySQL local

--- zoo-fantastico/src/main/java/com/example/zoo_fantastico/repository/CreatureRepository.java
+++ zoo-fantastico/src/main/java/com/example/zoo_fantastico/repository/CreatureRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.zoo_fantastico.entity.Creature;
 
 public interface CreatureRepository extends JpaRepository<Creature, Long>{
-    
+    //root user password: password
 }
